08f08032cea3ce1ff34fcab3a993a01754f13eca
urban-m <65124441+urban-m@users.noreply.github.com>
2020-05-22 11:33:34 +0200

    Add files via upload

--- script.R
+++ script.R
@@ -1,15 +1,15 @@
 #packages needed
 #tidyverse
 #brms
-#raster
-#rasterVis
+#OpenStreetMap
 #rgdal
+#sp
 
 require(tidyverse)
 require(brms)
-require(raster)
-require(rasterVis)
+require(OpenStreetMap)
 require(rgdal)
+require(sp)
 
 #check ob RTools drin ist
 Sys.getenv(“PATH”)
@@ -22,7 +22,7 @@ options(buildtools.check = function(action)TRUE)
 elevdata<-read.csv(file.choose(), header=T)
 elevdataupsid<-read.csv(file.choose(), header=T)
 
-#Mutationen f�rs Modelieren
+#Mutationen f?rs Modelieren
 elevdata <- mutate(elevdata, NonMarginal01 = as.logical(Nonmarginal_Uvular), NonMarginal01 = as.numeric(NonMarginal01))
 elevdata<- mutate(elevdata, NonMarginal02 = as.logical(Nonmarginal_Ejective), NonMarginal02 = as.numeric(NonMarginal02))
 elevdataupsid <- mutate(elevdataupsid, NonMarginal01 = as.logical(Nonmarginal_Uvular), NonMarginal01 = as.numeric(NonMarginal01))
@@ -52,7 +52,7 @@ plot(elevmodelejectives)
 fitejectives<-(fitted(elevmodelejectives, re_formula=NA, summary=TRUE))
 summary (fitejectives)
 
-#Modell für Ejektive, UPSID data (20000 Iterationen nötig, sonst Warnmeldung, au�erdem hier adapt_delta = 0.99 wg. divergent transitions after warmup bei adapt_delta = 0.8)
+#Modell für Ejektive, UPSID data (20000 Iterationen nötig, sonst Warnmeldung, au?erdem hier adapt_delta = 0.99 wg. divergent transitions after warmup bei adapt_delta = 0.8)
 elevmodelupsidejectives<-brm(NonMarginal02 ~ elevation + (1+ elevation| macroarea2) +(1+elevation|family_id), family= 'bernoulli', data=elevdataupsid, iter=20000, control = list(adapt_delta = 0.99))
 conditional_effects(elevmodelupsidejectives)
 summary(elevmodelupsidejectives)
@@ -61,13 +61,34 @@ fitejectivesupsid<-(fitted(elevmodelupsidejectives, re_formula=NA, summary=TRUE)
 summary (fitejectivesupsid)
 
 #plot maps
-relief_world <- raster(file.choose())
-plot(relief_world, box=F, axes=F, legend=F, col = grey(1:100/100))
+map <- openmap(c(80,-180), c(-70,180),type='nps', minNumTiles=100)
+plot(map)
 uvulars<-filter(elevdata, NonMarginal01==T)
 nonuvulars<-filter(elevdata, NonMarginal01==F)
-points(as.numeric(gsub(',','.',uvulars$longitude)), as.numeric(gsub(',','.',uvulars$latitude)), cex=0.8, pch=16, col="black", bg="black")
-points(as.numeric(gsub(',','.',nonuvulars$longitude)), as.numeric(gsub(',','.',nonuvulars$latitude)), cex=0.8, pch=16, col="white", bg="white")
+plotdatauvulars<-data.frame(lat=as.numeric(gsub(',','.',uvulars$latitude)), lon=as.numeric(gsub(',','.',uvulars$longitude)))
+plotdatauvulars<-plotdatauvulars[complete.cases(plotdatauvulars),]
+coordinates(plotdatauvulars)<-~lon+lat
+proj4string(plotdatauvulars)<-CRS("+init=epsg:4326")
+points(spTransform(plotdatauvulars,osm()), pch=21, col="white", bg="black", cex=1.2)
+
+plotdatanonuvulars<-data.frame(lat=as.numeric(gsub(',','.',nonuvulars$latitude)), lon=as.numeric(gsub(',','.',nonuvulars$longitude)))
+plotdatanonuvulars<-plotdatanonuvulars[complete.cases(plotdatanonuvulars),]
+coordinates(plotdatanonuvulars)<-~lon+lat
+proj4string(plotdatanonuvulars)<-CRS("+init=epsg:4326")
+points(spTransform(plotdatanonuvulars,osm()), pch=21, col="black", bg="white")
+
 ejectives<-filter(elevdata, NonMarginal02==T)
 nonejectives<-filter(elevdata, NonMarginal02==F)
-points(as.numeric(gsub(',','.',ejectives$longitude)), as.numeric(gsub(',','.',ejectives$latitude)), cex=0.8, col="white", bg="black")
-points(as.numeric(gsub(',','.',nonejectives$longitude)), as.numeric(gsub(',','.',nonejectives$latitude)), cex=0.8, col="white", bg="black")
+plotdataejectives<-data.frame(lat=as.numeric(gsub(',','.',ejectives$latitude)), lon=as.numeric(gsub(',','.',ejectives$longitude)))
+plotdataejectives<-plotdataejectives[complete.cases(plotdataejectives),]
+coordinates(plotdataejectives)<-~lon+lat
+proj4string(plotdataejectives)<-CRS("+init=epsg:4326")
+points(spTransform(plotdataejectives,osm()), pch=21, col="white", bg="black", cex=1.2)
+
+ejectives<-filter(elevdata, NonMarginal02==T)
+nonejectives<-filter(elevdata, NonMarginal02==F)
+plotdatanonejectives<-data.frame(lat=as.numeric(gsub(',','.',nonejectives$latitude)), lon=as.numeric(gsub(',','.',nonejectives$longitude)))
+plotdatanonejectives<-plotdatanonejectives[complete.cases(plotdatanonejectives),]
+coordinates(plotdatanonejectives)<-~lon+lat
+proj4string(plotdatanonejectives)<-CRS("+init=epsg:4326")
+points(spTransform(plotdatanonejectives,osm()), pch=21, col="black", bg="white")
